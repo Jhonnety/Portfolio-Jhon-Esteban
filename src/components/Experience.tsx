7dab72b4fd7567736429043ae31933a3acc630d4
Jhon Esteban Velasquez Gomez <jhonestebanvelasquezgomez@gmail.com>
2023-06-22 17:25:57 -0500

    Mejoras

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext} from "react";
 import { LanguageContext } from "../contexts";
 import { CARREAR, CARRERA, SEGUNDA_CARRERA, SECOND_CARREAR, EXPERIENCES } from "../utils/Data";
 
--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -2,7 +2,6 @@ import { Start } from "../components"
 import { Experience } from "../components/Experience"
 import { Profile } from "../components/Profile"
 import { Projects } from "../components/Projects"
-import { Prueba } from "../components/Prueba"
 
 export const Content = () => {
   return (
